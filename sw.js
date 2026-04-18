4b193d4ef8f04a55f65de99803f466b32486bbe6
Tipo4otkiy <glebbilouss@gmail.com>
2026-04-18 16:06:28 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,29 @@
-const CACHE = 'v3-ownerka';
+const CACHE = 'v4-dovidnyk';
 const ASSETS = ['./index.html', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
+    self.skipWaiting();
     e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
 
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
 self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+    e.respondWith(
+        fetch(e.request)
+            .then(res => {
+                const resClone = res.clone();
+                caches.open(CACHE).then(cache => cache.put(e.request, resClone));
+                return res;
+            })
+            .catch(() => caches.match(e.request))
+    );
 });
